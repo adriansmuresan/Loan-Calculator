e9bde0021c44ddefcea5c0b7888bed1b6fc17861
Adrian S MUresan <adriansmuresan@gmail.com>
2019-09-10 15:22:11 -0500

    Declares the variables that will grab thedayta for the function

--- app.js
+++ app.js
@@ -7,5 +7,15 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 function calculateResults(e){
   console.log('Calculating...');
 
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+
   e.preventDefault();
 }
